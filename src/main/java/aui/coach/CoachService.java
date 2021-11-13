baf3d4eadbd0e192cfc5240501615f2f73d70b85
Winetq <winetq9679@gmail.com>
2021-11-13 11:36:22 +0100

    Add event requests

--- src/main/java/aui/coach/CoachService.java
+++ src/main/java/aui/coach/CoachService.java
@@ -1,34 +1,42 @@
 package aui.coach;
 
+import aui.coach.event.CoachEventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
 class CoachService {
-    private final CoachRepository repository;
+    private final CoachRepository coachRepository;
+    private final CoachEventRepository eventRepository;
 
     @Autowired
-    CoachService(CoachRepository repository) {
-        this.repository = repository;
+    CoachService(CoachRepository coachRepository, CoachEventRepository eventRepository) {
+        this.coachRepository = coachRepository;
+        this.eventRepository = eventRepository;
     }
 
     Optional<Coach> find(Long id) {
-        return repository.findById(id);
+        return coachRepository.findById(id);
     }
 
     List<Coach> findAll() {
-        return repository.findAll();
+        return coachRepository.findAll();
     }
 
+    @Transactional
     void create(Coach entity) {
-        repository.save(entity);
+        eventRepository.create(entity);
+        coachRepository.save(entity);
     }
 
+    @Transactional
     void delete(Coach entity) {
-        repository.delete(entity);
+        eventRepository.delete(entity);
+        coachRepository.delete(entity);
     }
 }
 
---
+++ src/main/java/aui/coach/event/CoachEventRepository.java
@@ -0,0 +1,27 @@
+package aui.coach.event;
+
+import aui.coach.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.client.RestTemplate;
+
+@Repository
+public class CoachEventRepository {
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public CoachEventRepository(@Value("${lab3.swimmer.url}") String baseUrl) {
+        restTemplate = new RestTemplateBuilder().rootUri(baseUrl).build();
+    }
+
+    public void create(Coach coach) {
+        restTemplate.postForLocation("/coaches", CreateCoachRequest.entityToDto(coach)); // event request
+    }
+
+    public void delete(Coach coach) {
+        restTemplate.delete("/coaches/{id}", coach.getId()); // event request
+    }
+}
+
