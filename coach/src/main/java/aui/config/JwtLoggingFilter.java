0bf84f59de79bf231eb8d03c3c1219fa2fa7f623
Winetq <winetq9679@gmail.com>
2025-10-28 18:57:28 +0100

    Bump java to 21 and spring boot to 3.5.7 in coach service

--- coach/src/main/java/aui/config/JwtLoggingFilter.java
+++ coach/src/main/java/aui/config/JwtLoggingFilter.java
@@ -8,12 +8,12 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 import static java.time.Duration.between;
