b88724af97e6865b164db60d3c0e0d0073baa90b
Winetq <winetq9679@gmail.com>
2025-10-28 15:52:50 +0100

    Bump java to 21 and spring boot to 3.5.7

--- swimmer/src/main/java/aui/swimmer/Swimmer.java
+++ swimmer/src/main/java/aui/swimmer/Swimmer.java
@@ -4,15 +4,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-import static javax.persistence.EnumType.STRING;
-import static javax.persistence.GenerationType.IDENTITY;
+import static jakarta.persistence.EnumType.STRING;
+import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
 @Table(name = "swimmers")
